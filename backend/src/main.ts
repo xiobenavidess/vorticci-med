4a2db1bee57cbe8055993444c41e409d6b0bd2d3
Rocio Benavides <tu@email.com>
2026-06-16 19:25:48 -0400

    fix: agregar dominio vercel a CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,15 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://vorticci-med-app.vercel.app',
+      /\.vercel\.app$/,
+    ],
     credentials: true,
   })
-
   await app.listen(process.env.PORT || 4000)
 }
 bootstrap()
